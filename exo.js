99a9c38f3533d11c81c17018c2e1340a69845785
Luxanna23 <yasmineadh@gmail.com>
2024-02-15 12:00:03 +0100

    ok

---
+++ exo.js
@@ -0,0 +1,65 @@
+
+var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+// array.forEach(element => {
+//     console.log(element);
+// });
+
+
+var newReleases = [
+    {
+        "id": 70111470,
+        "title": "Die Hard",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id: 432534, time: 65876586 }]
+    },
+    {
+        "id": 65432445,
+        "title": "The Chamber",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+    },
+    {
+        "id": 675465,
+        "title": "Fracture",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id: 432534, time: 65876586 }]
+    }
+],
+videoAndTitlePairs = [];
+
+newReleases.forEach((element) => {
+    
+    videoAndTitlePairs.push({id : element.id, title : element.title});
+})
+
+videos = [];
+newReleases.forEach((element) => {
+    if (element.rating.includes( 5.0)){
+        videos.push(element);
+    }
+})
+
+const res = [];
+const result = newReleases.filter((video) => video.rating.includes( 5.0));
+
+    newReleases.filter((video) => {return video.rating == 5.0}).map((elem) => {res.push(elem.id)})
+console.log(res);
+
+const mapVideos = result.map((element) => element.id);
+
+// console.log(mapVideos);
